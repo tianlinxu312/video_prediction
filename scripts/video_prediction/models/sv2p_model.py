eb961c2a801df83f27c8ed689f73aefad8eb2d48
Lin <13871876+tianlinxu312@users.noreply.github.com>
2021-08-05 05:14:31 +0100

    Update sv2p_model.py

--- scripts/video_prediction/models/sv2p_model.py
+++ scripts/video_prediction/models/sv2p_model.py
@@ -27,234 +27,104 @@ from tensorflow.compat.v1 import layers as tf_layers
 from tf_slim import add_arg_scope
 from tf_slim import layers
 from video_prediction.models import VideoPredictionModel
-from tensorflow.compat.v1.python.keras.engine.base_layer import Layer
 
+
+from tensorflow.python.distribute import distribution_strategy_context
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
+from tensorflow.python.framework import tensor_shape
+from tensorflow.python.keras import backend
+from tensorflow.python.keras import constraints
+from tensorflow.python.keras import initializers
+from tensorflow.python.keras import regularizers
+from tensorflow.python.keras.engine.base_layer import Layer
+from tensorflow.python.keras.engine.input_spec import InputSpec
+from tensorflow.python.keras.utils import control_flow_util
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import init_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import nn
+from tensorflow.python.ops import state_ops
+from tensorflow.python.ops import variables as tf_variables
+from tensorflow.python.ops.control_flow_ops import get_enclosing_xla_context
+from tensorflow.python.platform import tf_logging as logging
+from tensorflow.python.util.tf_export import keras_export
 
 # Amount to use when lower bounding tensors
 RELU_SHIFT = 1e-12
 # kernel size for DNA and CDNA.
 DNA_KERN_SIZE = 5
 
-
-class LayerNormalization(Layer):
-  def __init__(self,
-               axis=-1,
-               epsilon=1e-3,
+@add_arg_scope
+def layer_norm(inputs,
                center=True,
                scale=True,
-               beta_initializer='zeros',
-               gamma_initializer='ones',
-               beta_regularizer=None,
-               gamma_regularizer=None,
-               beta_constraint=None,
-               gamma_constraint=None,
-               reuse=False,
-               **kwargs):
-    super(LayerNormalization, self).__init__(**kwargs)
-    if isinstance(axis, (list, tuple)):
-      self.axis = axis[:]
-    elif isinstance(axis, int):
-      self.axis = axis
-    else:
-      raise TypeError('Expected an int or a list/tuple of ints for the '
-                      'argument \'axis\', but received: %r' % axis)
-
-    self.epsilon = epsilon
-    self.center = center
-    self.scale = scale
-    self.beta_initializer = initializers.get(beta_initializer)
-    self.gamma_initializer = initializers.get(gamma_initializer)
-    self.beta_regularizer = regularizers.get(beta_regularizer)
-    self.gamma_regularizer = regularizers.get(gamma_regularizer)
-    self.beta_constraint = constraints.get(beta_constraint)
-    self.gamma_constraint = constraints.get(gamma_constraint)
-    self.reuse = reuse
-
-    self.supports_masking = True
-
-    # Indicates whether a faster fused implementation can be used. This will be
-    # set to True or False in build()"
-    self._fused = None
-
-  def _fused_can_be_used(self, ndims):
-    """Return false if fused implementation cannot be used.
-    Check if the axis is contiguous and can be collapsed into the last axis.
-    The self.axis is assumed to have no duplicates.
-    """
-    axis = sorted(self.axis)
-    can_use_fused = False
-
-    if axis[-1] == ndims - 1 and axis[-1] - axis[0] == len(axis) - 1:
-      can_use_fused = True
-
-    # fused_batch_norm will silently raise epsilon to be at least 1.001e-5, so
-    # we cannot used the fused version if epsilon is below that value. Also, the
-    # variable dtype must be float32, as fused_batch_norm only supports float32
-    # variables.
-    if self.epsilon < 1.001e-5 or self.dtype != 'float32':
-      can_use_fused = False
-
-    return can_use_fused
-
-  def build(self, input_shape):
-    ndims = len(input_shape)
-    if ndims is None:
-      raise ValueError('Input shape %s has undefined rank.' % input_shape)
-
-    # Convert axis to list and resolve negatives
-    if isinstance(self.axis, int):
-      self.axis = [self.axis]
-    elif isinstance(self.axis, tuple):
-      self.axis = list(self.axis)
-    for idx, x in enumerate(self.axis):
-      if x < 0:
-        self.axis[idx] = ndims + x
-
-    # Validate axes
-    for x in self.axis:
-      if x < 0 or x >= ndims:
-        raise ValueError('Invalid axis: %d' % x)
-    if len(self.axis) != len(set(self.axis)):
-      raise ValueError('Duplicate axis: {}'.format(tuple(self.axis)))
-
-    param_shape = [input_shape[dim] for dim in self.axis]
-    if self.scale:
-      self.gamma = self.add_weight(
-          name='gamma',
-          shape=param_shape,
-          initializer=self.gamma_initializer,
-          regularizer=self.gamma_regularizer,
-          constraint=self.gamma_constraint,
-          trainable=True,
-          experimental_autocast=False)
-    else:
-      self.gamma = None
-
-    if self.center:
-      self.beta = self.add_weight(
-          name='beta',
-          shape=param_shape,
-          initializer=self.beta_initializer,
-          regularizer=self.beta_regularizer,
-          constraint=self.beta_constraint,
-          trainable=True,
-          experimental_autocast=False)
-    else:
-      self.beta = None
-
-    self._fused = self._fused_can_be_used(ndims)
-
-    self.built = True
-
-  def call(self, inputs, reuse=False):
-    # Compute the axes along which to reduce the mean / variance
-    input_shape = inputs.shape
-    ndims = len(input_shape)
-
-    # Broadcasting only necessary for norm when the axis is not just
-    # the last dimension
-    broadcast_shape = [1] * ndims
-    for dim in self.axis:
-      broadcast_shape[dim] = input_shape.dims[dim].value
-
-    def _broadcast(v):
-      if (v is not None and len(v.shape) != ndims and self.axis != [ndims - 1]):
-        return array_ops.reshape(v, broadcast_shape)
-      return v
-
-    if not self._fused:
-      input_dtype = inputs.dtype
-      if input_dtype in ('float16', 'bfloat16') and self.dtype == 'float32':
-        # If mixed precision is used, cast inputs to float32 so that this is at
-        # least as numerically stable as the fused version.
-        inputs = math_ops.cast(inputs, 'float32')
-
-      # Calculate the moments on the last axis (layer activations).
-      mean, variance = nn.moments(inputs, self.axis, keep_dims=True)
-
-      scale, offset = _broadcast(self.gamma), _broadcast(self.beta)
-
-      # Compute layer normalization using the batch_normalization function.
-      outputs = nn.batch_normalization(
-          inputs,
-          mean,
-          variance,
-          offset=offset,
-          scale=scale,
-          variance_epsilon=self.epsilon)
-      outputs = math_ops.cast(outputs, input_dtype)
-    else:
-      # Collapse dims before self.axis, and dims in self.axis
-      pre_dim, in_dim = (1, 1)
-      axis = sorted(self.axis)
-      tensor_shape = array_ops.shape(inputs)
-      for dim in range(0, ndims):
-        dim_tensor = tensor_shape[dim]
-        if dim < axis[0]:
-          pre_dim = pre_dim * dim_tensor
-        else:
-          assert dim in axis
-          in_dim = in_dim * dim_tensor
-
-      squeezed_shape = [1, pre_dim, in_dim, 1]
-      # This fused operation requires reshaped inputs to be NCHW.
-      data_format = 'NCHW'
-
-      inputs = array_ops.reshape(inputs, squeezed_shape)
-
-      # self.gamma and self.beta have the wrong shape for fused_batch_norm, so
-      # we cannot pass them as the scale and offset parameters. Therefore, we
-      # create two constant tensors in correct shapes for fused_batch_norm and
-      # later construct a separate calculation on the scale and offset.
-      scale = array_ops.ones([pre_dim], dtype=self.dtype)
-      offset = array_ops.zeros([pre_dim], dtype=self.dtype)
-
-      # Compute layer normalization using the fused_batch_norm function.
-      outputs, _, _ = nn.fused_batch_norm(
-          inputs,
-          scale=scale,
-          offset=offset,
-          epsilon=self.epsilon,
-          data_format=data_format)
-
-      outputs = array_ops.reshape(outputs, tensor_shape)
-
-      scale, offset = _broadcast(self.gamma), _broadcast(self.beta)
-
-      if scale is not None:
-        outputs = outputs * math_ops.cast(scale, outputs.dtype)
-      if offset is not None:
-        outputs = outputs + math_ops.cast(offset, outputs.dtype)
-
-    # If some components of the shape got lost due to adjustments, fix that.
-    outputs.set_shape(input_shape)
-
-    return outputs
-
-  def compute_output_shape(self, input_shape):
-    return input_shape
-
-  def get_config(self):
-    config = {
-        'axis': self.axis,
-        'epsilon': self.epsilon,
-        'center': self.center,
-        'scale': self.scale,
-        'beta_initializer': initializers.serialize(self.beta_initializer),
-        'gamma_initializer': initializers.serialize(self.gamma_initializer),
-        'beta_regularizer': regularizers.serialize(self.beta_regularizer),
-        'gamma_regularizer': regularizers.serialize(self.gamma_regularizer),
-        'beta_constraint': constraints.serialize(self.beta_constraint),
-        'gamma_constraint': constraints.serialize(self.gamma_constraint),
-        'reuse': self.reuse
-    }
-    base_config = super(LayerNormalization, self).get_config()
-    return dict(list(base_config.items()) + list(config.items()))
-
-
-@add_arg_scope
-def layer_norm():
-    return LayerNormalization(axis=-1, name='ln', reuse=False)
+               activation_fn=None,
+               reuse=None,
+               variables_collections=None,
+               outputs_collections=None,
+               trainable=True,
+               begin_norm_axis=1,
+               begin_params_axis=-1,
+               scope=None):
+  with variable_scope.variable_scope(
+      scope, 'LayerNorm', [inputs], reuse=reuse) as sc:
+    inputs = ops.convert_to_tensor(inputs)
+    inputs_shape = inputs.shape
+    inputs_rank = inputs_shape.ndims
+    if inputs_rank is None:
+      raise ValueError('Inputs %s has undefined rank.' % inputs.name)
+    dtype = inputs.dtype.base_dtype
+    if begin_norm_axis < 0:
+      begin_norm_axis = inputs_rank + begin_norm_axis
+    if begin_params_axis >= inputs_rank or begin_norm_axis >= inputs_rank:
+      raise ValueError('begin_params_axis (%d) and begin_norm_axis (%d) '
+                       'must be < rank(inputs) (%d)' %
+                       (begin_params_axis, begin_norm_axis, inputs_rank))
+    params_shape = inputs_shape[begin_params_axis:]
+    if not params_shape.is_fully_defined():
+      raise ValueError(
+          'Inputs %s: shape(inputs)[%s:] is not fully defined: %s' %
+          (inputs.name, begin_params_axis, inputs_shape))
+    # Allocate parameters for the beta and gamma of the normalization.
+    beta, gamma = None, None
+    if center:
+      beta_collections = utils.get_variable_collections(variables_collections,
+                                                        'beta')
+      beta = variables.model_variable(
+          'beta',
+          shape=params_shape,
+          dtype=dtype,
+          initializer=init_ops.zeros_initializer(),
+          collections=beta_collections,
+          trainable=trainable)
+    if scale:
+      gamma_collections = utils.get_variable_collections(
+          variables_collections, 'gamma')
+      gamma = variables.model_variable(
+          'gamma',
+          shape=params_shape,
+          dtype=dtype,
+          initializer=init_ops.ones_initializer(),
+          collections=gamma_collections,
+          trainable=trainable)
+    # Calculate the moments on the last axis (layer activations).
+    norm_axes = list(range(begin_norm_axis, inputs_rank))
+    mean, variance = nn.moments(inputs, norm_axes, keep_dims=True)
+    # Compute layer normalization using the batch_normalization function.
+    variance_epsilon = 1e-12
+    outputs = nn.batch_normalization(
+        inputs,
+        mean,
+        variance,
+        offset=beta,
+        scale=gamma,
+        variance_epsilon=variance_epsilon)
+    outputs.set_shape(inputs_shape)
+    if activation_fn is not None:
+      outputs = activation_fn(outputs)
+    return utils.collect_named_outputs(outputs_collections, sc.name, outputs)
 
 
 def init_state(inputs,
