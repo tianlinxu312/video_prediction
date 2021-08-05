6547c2f7f1c3d36b7906adf0573bdd1e96a95022
Lin <13871876+tianlinxu312@users.noreply.github.com>
2021-08-05 03:52:16 +0100

    Update base_model.py

--- scripts/video_prediction/models/base_model.py
+++ scripts/video_prediction/models/base_model.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 
 import numpy as np
 import six
-import tensorflow as tf
+import tensorflowtf.compat.v1. as tf
 # from tensorflow.contrib.training import HParams
 from tensorflow.python.util import nest
 
