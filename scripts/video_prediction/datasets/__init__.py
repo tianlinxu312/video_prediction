1ab036a52faf6b6184c6e97e1b6d9bdafce2beb6
Lin <13871876+tianlinxu312@users.noreply.github.com>
2021-08-05 01:26:00 +0100

    Update __init__.py

--- scripts/video_prediction/datasets/__init__.py
+++ scripts/video_prediction/datasets/__init__.py
@@ -20,6 +20,6 @@ def get_dataset_class(dataset):
     }
     dataset_class = dataset_mappings.get(dataset, dataset)
     dataset_class = globals().get(dataset_class)
-    if dataset_class is None or not issubclass(dataset_class, BaseVideoDataset):
-        raise ValueError('Invalid dataset %s' % dataset)
+    # if dataset_class is None or not issubclass(dataset_class, BaseVideoDataset):
+    #     raise ValueError('Invalid dataset %s' % dataset)
     return dataset_class
